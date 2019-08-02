db77fdef0c1df4ce31bc357f57eb3ab3b250a0e1
cdot <git@c-dot.co.uk>
2019-08-02 08:16:35 +0100

    Added local cache for samples to avoid repeated reads from webdav

--- sensors/js/SampleStore.js
+++ sensors/js/SampleStore.js
@@ -20,6 +20,9 @@ define("js/SampleStore", [ "webdav" ], function(WebDAV) {
                     username: user,
                     password: pass
                 });
+
+            // Keep a cache of what was last written for each sensor ID
+            this.mCaches = {};
         }
 
         /**
@@ -30,42 +33,53 @@ define("js/SampleStore", [ "webdav" ], function(WebDAV) {
          * @return Promise that will resolve when the sample has been saved
          */
         addSample(data, age_limit, id) {
-            let path = id + ".csv";
             let cutoff = Date.now() - age_limit;
-            // SMELL: npm webdav doesn't provide any locking mechanism.
-            // Not a big deal, as the architecture can't cause collisions.
-            return this.mDAV.getFileContents(path, { format:"text" })
-            .catch((e) => {
-                console.error("Failed to read", path, "from", this.mURL);
-                return "";
-            })
-            .then((res) => {
-                // Split into lines, and then into columns. Each row has
-                // two columns, time and sample.
-                let records;
-                if (res) {
-                    let lines = res.toString().split("\n");
-                    records = lines.map(s => s.split(","));
-                } else
-                    records = [];
-                // Parse columns
-                for (let r of records) {
-                    r[0] = parseNumber(r[0]);
+            let promise;
+            if (this.mCaches[id])
+                promise = Promise.resolve(this.mCaches[id]);
+            else {
+                // Need to initialise internal cache from webdav
+                let path = id + ".csv";
+                promise =  this.mDAV.getFileContents(path, { format:"text" })
+                .catch((e) => {
+                    console.error("Failed to read", path, "from", this.mURL);
+                    this.mCaches[id] = [];
+                    return this.mCaches[id];
+                })
+                .then((res) => {
+                    // Split into lines, and then into columns. Each row has
+                    // two columns, time and sample.
+                    let records;
+                    if (res) {
+                        let lines = res.toString().split("\n");
+                        records = lines.map(s => s.split(","));
 
-                    // Filter records by age
-                    while (records[0][0] < cutoff)
-                        records.shift();
-                }
+                        // Parse columns
+                        for (let r of records) {
+                            r[0] = parseInt(r[0]); // time
+                            r[1] = parseFloat(r[1]); // sample
+                        }
+                        this.mCaches[id] = records;
+                    } else
+                        this.mCaches[id] = [];
 
-                console.debug("Adding sample");
+                    return this.mCaches[id];
+                });
+            }
+
+            return promise.then((records) => {
+                // Filter records by age
+                while (records.length > 0 && records[0][0] < cutoff)
+                    records.shift();
+
                 // Add new sample
-                records.push([Date.now(), samples[sensor]]);
+                records.push([Date.now(), data]);
 
                 // Recombine into a string
                 let body = records.map(r => r.join(",")).join("\n");
 
                 // And save the new body
-                return this.mDAV.putFileContents(this.mPath,  body)
+                return this.mDAV.putFileContents(path,  body)
                 .catch((e) => {
                     // WebDAV errors don't kill the service
                     console.error("Failed to update", path, "on", this.mURL, e);
