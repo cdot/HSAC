30a568e9757f8a3bed07f0a0f190ff36226d117a
cdot <git@c-dot.co.uk>
2019-09-02 12:17:27 +0100

    Make power incremental

--- sensors/js/SimulatedSensor.js
+++ sensors/js/SimulatedSensor.js
@@ -3,23 +3,28 @@
 define("js/SimulatedSensor", ["js/Sensor", "js/Time"], function(Sensor, Time) {
 
     class SimulatedSensor extends Sensor {
+        
         constructor(config) {
             console.log("Creating simulated", config.name);
             super(config);
             if (config.class === "DHTxx") {
-                if (config.field === "temperature") {
-                    this.min = -5, this.max = 41;
-                } else {
-                    this.min = 0, this.max = 100;
+                if (config.field === "temperature")
+                    this.resample = () => 46 * Math.random() - 5;
+                else
+                    this.resample = () => 100 * Math.random();
+            } else if (config.class === "Power") {
+                this.resample = () => {
+                    // Power on time since last sample - 3s
+                    return Math.floor(Math.random() * 6000);
                 }
-            } else {
-                this.min = 20; this.max = 91;
-            }
+            } else
+                this.resample = () => 71 * Math.random() + 20;
         }
 
         sample() {
-            this.val = this.min + (this.max - this.min) * Math.random();
-            return Promise.resolve({ sample: this.val, time: Time.now() });
+            return Promise.resolve({
+                sample: this.resample(), time: Time.now()
+            });
         }
     }
 
